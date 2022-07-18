3d572c915a089a991f586f5cc2cde10ad004ccf2
ramses2099 <ramses2099@gmail.com>
2022-07-18 19:56:21 -0400

    first commit

---
+++ src/main/java/com/jprograming/webapispringboot/WebapispringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jprograming.webapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapispringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jprograming/webapispringboot/WebapispringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jprograming.webapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapispringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
